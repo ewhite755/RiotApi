22194672d4b4855839c1975e7db69109ba95081d
ewhite755 <54605007+ewhite755@users.noreply.github.com>
2019-11-26 19:09:57 -0500

    riot api testing

---
+++ RiotAPIDemo/Summoner.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+namespace RiotAPIDemo
+{
+    public class Summoner
+    {
+        [JsonProperty("id")]
+        public string ID { get; set; }
+
+        [JsonProperty("accountId")]
+        public string AccountID { get; set; }
+
+        [JsonProperty("puuid")]
+        public string PuuId { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("profileIconId")]
+        public int ProfileIconId { get; set; }
+
+        [JsonProperty("revisionDate")]
+        public long RevisionDate { get; set; }
+
+        [JsonProperty("summonerLevel")]
+        public long Level { get; set; }
+
+    }
+}
+
+    //"id": "ChExhAUCl08qDgq6z6mHcyKQ2PJfJH5XSmdu8D6EKa554lk",
+    //"accountId": "MXYElr-RLAMaGQnWOg8pacIMApgs58k9lfE2DaX3BSUuv3o",
+    //"puuid": "JQkdopdkS3bdGdIGFB-kkJ3x9UVniHZ6lLy5f9qiyVUbuD6-bOasEPNI3CqfCzX2SVk-eT6ijZPE3A",
+    //"name": "reg454",
+    //"profileIconId": 691,
+    //"revisionDate": 1574324004000,
+    //"summonerLevel": 69
---
+++ RiotAPIDemo/SummonerV4.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace RiotAPIDemo
+{
+    class SummonerV4
+    {
+
+        public static readonly string ApiKey = "RGAPI-b68e4c84-c788-49b1-9d22-3fe64b301064";
+
+        public static readonly Uri ApiBaseAddress = new Uri("https://na1.api.riotgames.com/lol/summoner/v4/summoners");
+        public HttpClient Client { get; set; }
+
+        public SummonerV4()
+        {
+            Client = new HttpClient
+            {
+                BaseAddress = ApiBaseAddress
+            };
+        }
+
+        public Summoner GetSummonerByName(string summonerName)
+        {
+            var uriBuilder = new UriBuilder($"{ApiBaseAddress}/by-name/{summonerName}");
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            query["api_key"] = ApiKey;
+            uriBuilder.Query = query.ToString();
+            string requestUri = uriBuilder.ToString();
+
+            var response = Client.GetAsync(requestUri).Result;
+            if (response.IsSuccessStatusCode == false)
+            {
+                throw new Exception($"Request failed: {response.StatusCode}");
+            }
+
+            string jsonResponse = response.Content.ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<Summoner>(jsonResponse);
+        }
+    }
+}
